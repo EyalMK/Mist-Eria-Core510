77228e6f33e6a30b9b4fd5500ea0e0d47ba93f85
Terah <terah.dev@gmail.com>
2013-08-09 14:49:53 +0200

    Fix Ranged Slot

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -669,10 +669,10 @@ enum EquipmentSlots                                         // 19 slots
     EQUIPMENT_SLOT_FINGER2      = 11,
     EQUIPMENT_SLOT_TRINKET1     = 12,
     EQUIPMENT_SLOT_TRINKET2     = 13,
-    EQUIPMENT_SLOT_BACK         = 14,
+    EQUIPMENT_SLOT_BACK         = 14, 
     EQUIPMENT_SLOT_MAINHAND     = 15,
     EQUIPMENT_SLOT_OFFHAND      = 16,
-    EQUIPMENT_SLOT_RANGED       = 17,
+    EQUIPMENT_SLOT_RANGED       = 15,
     EQUIPMENT_SLOT_TABARD       = 18,
     EQUIPMENT_SLOT_END          = 19
 };
