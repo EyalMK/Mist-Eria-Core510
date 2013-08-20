d627f7711217f379377a7999cdfe291d889020c0
Pexirn <pexirn.dev@gmail.com>
2013-08-20 22:55:23 +0200

    more gossip logs

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -14326,9 +14326,14 @@ void Player::PrepareGossipMenu(WorldObject* source, uint32 menuId /*= 0*/, bool 
                     break;
                 }
                 case GOSSIP_OPTION_TRAINER:
+				{
                     if (!creature->isCanTrainingOf(this, false))
+					{
                         canTalk = false;
+						if(menuId == 581) sLog->outInfo(LOG_FILTER_SERVER_LOADING, "PEXIRN : GOSSIP : WHAAAAAAAAAT");
+					}
                     break;
+				}
                 case GOSSIP_OPTION_LEARNDUALSPEC:
                     if (!(GetSpecsCount() == 1 && creature->isCanTrainingAndResetTalentsOf(this) && !(getLevel() < sWorld->getIntConfig(CONFIG_MIN_DUALSPEC_LEVEL))))
                         canTalk = false;
