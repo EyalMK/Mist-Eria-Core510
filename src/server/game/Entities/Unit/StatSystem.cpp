1549d678aa4dd87c4917fae8ad59955641d7892a
Pexirn <pexirn.dev@gmail.com>
2013-08-20 17:43:37 +0200

    WTF

--- src/server/game/Entities/Unit/StatSystem.cpp
+++ src/server/game/Entities/Unit/StatSystem.cpp
@@ -593,7 +593,7 @@ void Player::UpdateMastery(int32 amount)
 {
 	float mastery = (getLevel() >= 80) ? 8.0f : 0.0f;
 	if(GetActiveSpec() == TALENT_TREE_WARRIOR_FURY || GetActiveSpec() == TALENT_TREE_MAGE_FROST) mastery = 2.0f;
-
+	mastery = 0.0f;
 	float mastery_to_add = amount * GetRatingMultiplier(CR_MASTERY);
 	mastery += mastery_to_add;
 
