46eb4de1985daf5880c49cf68f8ec38133046965
Cristian Vlad <bordeianu.vlad@yahoo.com>
2013-08-20 08:33:41 +0000

    PetHandler.cpp edited online with Bitbucket ( learn_talent pet opcode struct )

--- src/server/game/Handlers/PetHandler.cpp
+++ src/server/game/Handlers/PetHandler.cpp
@@ -866,11 +866,30 @@ void WorldSession::HandlePetLearnTalent(WorldPacket& recvData)
 {
     sLog->outDebug(LOG_FILTER_NETWORKIO, "WORLD: CMSG_PET_LEARN_TALENT");
 
-    uint64 guid;
-    uint32 talentId, requestedRank;
-    recvData >> guid >> talentId >> requestedRank;
+    ObjectGuid guid;
+    uint32 talentId;
 
-    _player->LearnPetTalent(guid, talentId, requestedRank);
+    recvData >> talentId;
+
+	guid[4] = recvData.ReadBit();
+    guid[2] = recvData.ReadBit();
+    guid[5] = recvData.ReadBit();
+    guid[0] = recvData.ReadBit();
+    guid[3] = recvData.ReadBit();
+    guid[6] = recvData.ReadBit();
+    guid[1] = recvData.ReadBit();
+    guid[7] = recvData.ReadBit();
+
+    recvData.ReadByteSeq(guid[5]);
+    recvData.ReadByteSeq(guid[0]);
+    recvData.ReadByteSeq(guid[1]);
+    recvData.ReadByteSeq(guid[3]);
+    recvData.ReadByteSeq(guid[4]);
+    recvData.ReadByteSeq(guid[6]);
+    recvData.ReadByteSeq(guid[2]);
+    recvData.ReadByteSeq(guid[7]);
+
+    _player->LearnPetTalent(guid, talentId);
     _player->SendTalentsInfoData(true);
 }
 
