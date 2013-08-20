48beba1d5238137cf4f8a37d1e23b0e31baee4c4
Pexirn <pexirn.dev@gmail.com>
2013-08-20 17:19:23 +0200

    work on mastery

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -3250,8 +3250,8 @@ void Player::InitStatsForLevel(bool reapplyMods)
     for (uint16 index = PLAYER_FIELD_COMBAT_RATING_1; index < PLAYER_FIELD_COMBAT_RATING_1 + MAX_COMBAT_RATING; ++index)
         SetUInt32Value(index, 0);
 
-	float mastery = getLevel() >= 80 ? 8.0f : 0.0f;
-	SetFloatValue(PLAYER_MASTERY, mastery);
+	//float mastery = getLevel() >= 80 ? 8.0f : 0.0f;
+	SetFloatValue(PLAYER_MASTERY, 0.0f/*mastery*/);
 
     SetUInt32Value(PLAYER_FIELD_MOD_HEALING_DONE_POS, 0);
     SetFloatValue(PLAYER_FIELD_MOD_HEALING_PCT, 1.0f);
@@ -17320,13 +17320,14 @@ bool Player::LoadFromDB(uint32 guid, SQLQueryHolder *holder)
 
     SetSpecsCount(fields[53].GetUInt8());
     SetActiveSpec(fields[54].GetUInt8());
+	/*
 	if(getLevel() >= 80)
 	{
 		float mastery = 8.0f;
 		if(fields[54].GetUInt8() == TALENT_TREE_MAGE_FROST || fields[54].GetUInt8() == TALENT_TREE_WARRIOR_FURY) mastery = 2.0f;
 		SetFloatValue(PLAYER_MASTERY, mastery);
 	}
-
+	*/
     // sanity check
     if (GetSpecsCount() > MAX_TALENT_SPECS || GetActiveSpec() > MAX_TALENT_SPEC || GetSpecsCount() < MIN_TALENT_SPECS)
     {
@@ -25820,12 +25821,14 @@ void Player::ActivateSpec(uint8 spec)
                 RemoveAurasDueToSpell(old_gp->SpellId);
 
     SetActiveSpec(spec);
+	/*
 	if(getLevel() >= 80)
 	{
 		float mastery = 8.0f;
 		if(spec == TALENT_TREE_MAGE_FROST || spec == TALENT_TREE_WARRIOR_FURY) mastery = 2.0f;
 		SetFloatValue(PLAYER_MASTERY, mastery);
 	}
+	*/
     uint32 spentTalents = 0;
 
     specSpells = GetSpecializationSpellsBySpec(GetPrimaryTalentTree(GetActiveSpec()));
