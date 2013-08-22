baa9e8653631c93a96c3eafa3255c03dee628362
Pexirn <pexirn.dev@gmail.com>
2013-08-22 17:07:48 +0200

    fix SMSG_LOOT_REMOVED

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -9308,7 +9308,27 @@ void Player::SendNotifyLootMoneyRemoved()
 void Player::SendNotifyLootItemRemoved(uint8 lootSlot)
 {
     WorldPacket data(SMSG_LOOT_REMOVED, 1);
-    data << uint8(lootSlot);
+	ObjectGuid guid = this->GetLootGUID();
+
+	data.WriteBit(guid[1]);
+	data.WriteBit(guid[3]);
+	data.WriteBit(guid[4]);
+	data.WriteBit(guid[0]);
+	data.WriteBit(guid[6]);
+	data.WriteBit(guid[5]);
+	data.WriteBit(guid[2]);
+	data.WriteBit(guid[7]);
+
+	data.WriteByteSeq(guid[5]);
+	data.WriteByteSeq(guid[6]);
+	data.WriteByteSeq(guid[4]);
+	data << uint8(lootSlot);
+	data.WriteByteSeq(guid[0]);
+	data.WriteByteSeq(guid[3]);
+	data.WriteByteSeq(guid[2]);
+	data.WriteByteSeq(guid[7]);
+	data.WriteByteSeq(guid[1]);
+    
     GetSession()->SendPacket(&data);
 }
 
