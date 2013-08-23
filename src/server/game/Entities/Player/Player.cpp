dc89386be2f0c126d0203f3a55374cd727b6807b
Pexirn <pexirn.dev@gmail.com>
2013-08-23 15:54:52 +0200

    fix SMSG_LOOT_RESPONSE #2

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -9254,10 +9254,9 @@ void Player::SendLoot(uint64 guid, LootType loot_type)
     for(int i = 0 ; i < loot->items.size() ; i++)
     {
         LootItem item = loot->items[i];
-		/*
+		
         if(i)
-            data << uint8(LOOT_SLOT_TYPE_MASTER);
-			*/
+            data << uint8(LOOT_SLOT_TYPE_ALLOW_LOOT);
         data << uint32(0);
         data << uint32(item.count);
         data << uint32(0);
