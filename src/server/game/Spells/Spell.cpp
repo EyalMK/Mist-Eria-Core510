a384b411c09e3efbd2e8eb6940440589bf38d726
Terah <terah.dev@gmail.com>
2013-08-31 22:21:44 +0200

    Add logs

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -3740,6 +3740,8 @@ void Spell::finish(bool ok)
 
 void Spell::SendCastResult(SpellCastResult result)
 {
+	sLog->outDebug(LOG_FILTER_NETWORKIO, "Send cast result %u", uint32(result));
+
     if (result == SPELL_CAST_OK)
         return;
 
