8d58d41cfc597331ee2e4becd6f40fa04e6664d0
Pexirn <pexirn.dev@gmail.com>
2013-09-30 03:40:35 +0200

    try fix currency

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -7345,13 +7345,10 @@ void Player::SendNewCurrency(uint32 id) const
 
 void Player::SendCurrencies() const
 {
-/*
+    WorldPacket packet(SMSG_INIT_CURRENCY);
     ByteBuffer currencyData;
-    WorldPacket packet(SMSG_INIT_CURRENCY, 4 + _currencyStorage.size()*(5*4 + 1));
 
-	packet.WriteBits(_currencyStorage.size(), 22);
-    size_t count_pos = packet.bitwpos();
-
+    packet.WriteBits(_currencyStorage.size(), 22);
     size_t count = 0;
     for (PlayerCurrenciesMap::const_iterator itr = _currencyStorage.begin(); itr != _currencyStorage.end(); ++itr)
     {
@@ -7365,12 +7362,12 @@ void Player::SendCurrencies() const
         uint32 weekCount = itr->second.weekCount / precision;
         uint32 weekCap = GetCurrencyWeekCap(entry) / precision;
 
-		packet.WriteBits(0, 5); // some flags
+        packet.WriteBits(0, 5); // some flags
         packet.WriteBit(weekCap);
         packet.WriteBit(0);     // season total earned
         packet.WriteBit(weekCount);
 
-		currencyData << uint32(entry->ID);
+        currencyData << uint32(entry->ID);
         currencyData << uint32(itr->second.totalCount / precision);
 
         if (weekCap)
@@ -7387,9 +7384,9 @@ void Player::SendCurrencies() const
 
     packet.FlushBits();
     packet.append(currencyData);
-    packet.PutBits(count_pos, count, 22);
+    packet.PutBits(0, count, 22);
+
     GetSession()->SendPacket(&packet);
-*/
 }
 
 void Player::SendPvpRewards() const
