124cc8bd56e1abdae60fc6b8a51f625657f5a218
nobodie <nobodie.dev@gmail.com>
2013-09-30 02:23:10 +0200

    queueslot

--- src/server/game/Battlegrounds/BattlegroundMgr.cpp
+++ src/server/game/Battlegrounds/BattlegroundMgr.cpp
@@ -222,10 +222,10 @@ void BattlegroundMgr::BuildBattlegroundStatusPacket(WorldPacket* data, Battlegro
             data->WriteByteSeq(playerGuid[6]);
             data->WriteByteSeq(bgGuid[7]);
 			data->WriteByteSeq(bgGuid[1]);
-            *data << uint32(Time1);                     // Estimated Wait Time
+            *data << uint32(QueueSlot);                     // Estimated Wait Time time1
             *data << uint8(bg->GetMinLevel());          // Min Level   
             data->WriteByteSeq(bgGuid[4]);
-			*data << uint32(Time2); 
+            *data << uint32(QueueSlot);  //time2
             data->WriteByteSeq(bgGuid[5]);
             data->WriteByteSeq(playerGuid[3]);
             *data << uint32(QueueSlot);
@@ -238,7 +238,7 @@ void BattlegroundMgr::BuildBattlegroundStatusPacket(WorldPacket* data, Battlegro
             *data << uint32(GetMSTimeDiffToNow(Time2)); // Time since joined
             data->WriteByteSeq(bgGuid[2]);
             *data << uint8(bg->GetMaxLevel());                          // unk                   
-            *data << uint32(bg->GetClientInstanceID()); // Client Instance ID
+            *data << uint32(QueueSlot); // Client Instance ID instanceID
             break;
         }
 		case STATUS_WAIT_JOIN:
