a82ac939b19be9df2c860514af4b2be1ad45db7b
Terah <terah.dev@gmail.com>
2013-10-05 15:55:37 +0200

    Test portail

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3676,6 +3676,9 @@ void SpellMgr::LoadSpellInfoCorrections()
             case 126892:
                 spellInfo->CategoryRecoveryTime = 0;
                 break;
+			case 32268:
+				spellInfo->Effects[0].Effect = SPELL_EFFECT_TELEPORT_UNITS;
+				break;
             default:
                 break;
         }
