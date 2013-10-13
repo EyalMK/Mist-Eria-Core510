1013fc509a7d973ad5ce1d8b461dfe6f5b8bea63
nobodie <nobodie.dev@gmail.com>
2013-10-13 23:36:57 +0200

    dfdsfsdfs

--- src/server/game/Handlers/MiscHandler.cpp
+++ src/server/game/Handlers/MiscHandler.cpp
@@ -2103,7 +2103,7 @@ void WorldSession::HandleObjectUpdateFailedOpcode(WorldPacket& recvPacket)
     recvPacket.ReadByteSeq(guid[6]);
 
     WorldObject* obj = ObjectAccessor::GetWorldObject(*GetPlayer(), guid);    
-    sLog->outError(LOG_FILTER_NETWORKIO, "Object update failed for object " UI64FMTD " (%s) for player %s (%u)", uint64(guid), obj ? obj->GetName().c_str() : "object-not-found", GetPlayerName().c_str(), GetGuidLow());
+    sLog->outDebug(LOG_FILTER_NETWORKIO, "Object update failed for object " UI64FMTD " (%s) for player %s (%u)", uint64(guid), obj ? obj->GetName().c_str() : "object-not-found", GetPlayerName().c_str(), GetGuidLow());
 
     WorldPacket delObj(SMSG_DESTROY_OBJECT);
     delObj << uint64(guid);
