0707b6e35ad8615a2a0a3706b897874a23f57b7e
Terah <terah.dev@gmail.com>
2013-10-19 00:48:32 +0200

    Same

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -6753,10 +6753,8 @@ void Player::CheckUndermap() // WIP
 		h = GetMap()->GetHeight(x, y, h-2);
 	}
 
-	sLog->outDebug(LOG_FILTER_NETWORKIO, "CHECK UNDERMAP # x : %f; y : %f; z : %f; h : %f", x, y, z, h);
-
 	if (h > z + diff)
-		TeleportTo(GetMapId(), x, y, h + 1.f, GetOrientation());
+		TeleportTo(GetMapId(), x, y, h + diff, GetOrientation());
 }
 
 void Player::SaveRecallPosition()
