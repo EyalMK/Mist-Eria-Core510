845c5b8f2b681daba08c21c6918b366c51a52571
Terah <terah.dev@gmail.com>
2013-10-26 16:24:08 +0200

    Work on guild bank tab

--- src/server/game/Guilds/Guild.cpp
+++ src/server/game/Guilds/Guild.cpp
@@ -3429,8 +3429,8 @@ void Guild::SendBankList(WorldSession* session, uint8 tabId, bool withContent, b
             {
                 if (Item* tabItem = tab->GetItem(slotId))
                 {
-					data << uint32(1000);
-                    data << uint32(2000);
+					data << uint32(0);
+                    data << uint32(0);
                     data << uint32(slotId);
 
 					for (uint32 ench = 0; ench < MAX_ENCHANTMENT_SLOT; ++ench)
@@ -3444,11 +3444,11 @@ void Guild::SendBankList(WorldSession* session, uint8 tabId, bool withContent, b
 
 					data << uint32(0); // GetDataInSitu with this size
 
-					data << uint32(60);
+					data << uint32(tabItem->GetEntry());
 					data << uint32(50);
 					data << uint32(40);
 					data << uint32(30);
-					data << uint32(20);
+					data << uint32(tabItem->GetCount());
 					data << uint32(10);
 
 					/*
