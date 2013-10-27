9cf8efd17c5bd8bb87f9b492955366eb62b26fea
nobodie <nobodie.dev@gmail.com>
2013-10-27 21:42:59 +0100

    reverting modifications for difficulty opcodes

--- src/server/game/Entities/Creature/Creature.h
+++ src/server/game/Entities/Creature/Creature.h
@@ -585,7 +585,7 @@ class Creature : public Unit, public GridObject<Creature>, public MapCreature
         bool LoadCreatureFromDB(uint32 guid, Map* map, bool addToMap = true);
         void SaveToDB();
                                                             // overriden in Pet
-        virtual void SaveToDB(uint32 mapid, uint8 spawnMask, uint32 phaseMask);
+        virtual void SaveToDB(uint32 mapid, uint32 spawnMask, uint32 phaseMask);
         virtual void DeleteFromDB();                        // overriden in Pet
 
         Loot loot;
