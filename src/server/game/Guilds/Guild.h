cd68420e08ebbaf9fd205baf13284aef77b72935
Terah <terah.dev@gmail.com>
2013-10-27 15:37:49 +0100

    Test guild event

--- src/server/game/Guilds/Guild.h
+++ src/server/game/Guilds/Guild.h
@@ -165,7 +165,7 @@ enum GuildEvents
     GE_BANK_TAB_PURCHASED               = 19,
     GE_BANK_TAB_UPDATED                 = 20,
     GE_BANK_MONEY_SET                   = 21,
-    GE_BANK_MONEY_CHANGED               = 22,
+    GE_BANK_MONEY_CHANGED               = 23,
     GE_BANK_TEXT_CHANGED                = 23,
     // 24 - error 795
     GE_SIGNED_ON_MOBILE                 = 25,
