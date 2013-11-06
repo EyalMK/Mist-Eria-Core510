024bcfe1b1f63838ce78c2e2c8f09acd57596202
aaaaaa <anthonygg@hotmail.fr>
2013-11-06 17:37:26 +0100

    Fix for spell_pal_templar_s_verdict

--- src/server/scripts/Spells/spell_paladin.cpp
+++ src/server/scripts/Spells/spell_paladin.cpp
@@ -877,7 +877,7 @@ class spell_pal_templar_s_verdict : public SpellScriptLoader
             {                
                 Unit* caster = GetCaster();
 				uint8 level = caster->getLevel();
-				int32 damage = int32((level*(19 - 0.134f*(level-1))) + caster->GetTotalAttackPowerValue(BASE_ATTACK) * (275.0f /100.0f) / 100.0f);
+				int32 damage = int32((level*(19 - 0.134f*(level-1))) + (caster->GetTotalAttackPowerValue(BASE_ATTACK) * (275.0f /100.0f) / 60.0f));
                 SetHitDamage(damage);
             }
 
