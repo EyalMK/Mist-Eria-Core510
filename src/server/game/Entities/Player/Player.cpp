6fd866b31cd5c8b1ca12c9f567fa91e53ed5863c
Cristian Vlad <bordeianu.vlad@yahoo.com>
2013-11-07 17:46:46 +0200

    SendMovementSetWaterWalking new struct.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -26698,46 +26698,46 @@ void Player::SendMovementSetWaterWalking(bool apply)
     if (apply)
     {
         data.Initialize(SMSG_MOVE_WATER_WALK, 1 + 4 + 8);
-        data.WriteBit(guid[4]);
-        data.WriteBit(guid[7]);
-        data.WriteBit(guid[6]);
-        data.WriteBit(guid[0]);
-        data.WriteBit(guid[1]);
+        data.WriteBit(guid[2]);
         data.WriteBit(guid[3]);
         data.WriteBit(guid[5]);
-        data.WriteBit(guid[2]);
+        data.WriteBit(guid[0]);
+        data.WriteBit(guid[6]);
+        data.WriteBit(guid[7]);
+        data.WriteBit(guid[1]);
+        data.WriteBit(guid[4]);
 
-        data.WriteByteSeq(guid[0]);
-        data.WriteByteSeq(guid[5]);
-        data.WriteByteSeq(guid[2]);
-        data << uint32(0);          //! movement counter
         data.WriteByteSeq(guid[7]);
-        data.WriteByteSeq(guid[3]);
-        data.WriteByteSeq(guid[4]);
         data.WriteByteSeq(guid[1]);
+        data.WriteByteSeq(guid[3]);
+        data << uint32(0);          //! movement counter
+        data.WriteByteSeq(guid[2]);
+        data.WriteByteSeq(guid[0]);
         data.WriteByteSeq(guid[6]);
+        data.WriteByteSeq(guid[4]);
+        data.WriteByteSeq(guid[5]);
     }
     else
     {
         data.Initialize(SMSG_MOVE_LAND_WALK, 1 + 4 + 8);
-        data.WriteBit(guid[5]);
-        data.WriteBit(guid[1]);
-        data.WriteBit(guid[6]);
-        data.WriteBit(guid[2]);
-        data.WriteBit(guid[3]);
-        data.WriteBit(guid[4]);
         data.WriteBit(guid[0]);
         data.WriteBit(guid[7]);
+        data.WriteBit(guid[5]);
+        data.WriteBit(guid[3]);
+        data.WriteBit(guid[1]);
+        data.WriteBit(guid[6]);
+        data.WriteBit(guid[4]);
+        data.WriteBit(guid[2]);
 
-        data.WriteByteSeq(guid[6]);
-        data.WriteByteSeq(guid[1]);
+        data.WriteByteSeq(guid[2]);
         data.WriteByteSeq(guid[7]);
+		data << uint32(0);          //! movement counter
+        data.WriteByteSeq(guid[3]);
+        data.WriteByteSeq(guid[1]);
+        data.WriteByteSeq(guid[0]);
+        data.WriteByteSeq(guid[6]);
         data.WriteByteSeq(guid[5]);
         data.WriteByteSeq(guid[4]);
-        data.WriteByteSeq(guid[0]);
-        data.WriteByteSeq(guid[3]);
-        data.WriteByteSeq(guid[2]);
-        data << uint32(0);          //! movement counter
     }
     SendDirectMessage(&data);
 }
