992f9f71f5e06e5f3fb51110f48a6e6d29d67651
nobodie <nobodie.dev@gmail.com>
2013-11-14 21:21:25 +0100

    testing some mod about party and raid chats

--- src/server/game/Chat/Chat.cpp
+++ src/server/game/Chat/Chat.cpp
@@ -690,7 +690,7 @@ void ChatHandler::FillMessageData(WorldPacket* data, WorldSession* session, uint
     }
 
     *data << uint64(target_guid);                           // there 0 for BG messages
-    *data << uint32(0);                                     // can be chat msg group or something
+    *data << uint32(CHAT_MSG_PARTY || CHAT_MSG_PARTY_LEADER || CHAT_MSG_RAID);                                     // can be chat msg group or something
 
     if (type == CHAT_MSG_CHANNEL)
     {
@@ -705,6 +705,10 @@ void ChatHandler::FillMessageData(WorldPacket* data, WorldSession* session, uint
     }
     else
         *data << uint64(target_guid);
+
+    if (type == CHAT_MSG_PARTY || type == CHAT_MSG_RAID) {
+        *data << uint64(target_guid);
+    }
 
     *data << uint32(messageLength);
     *data << message;
