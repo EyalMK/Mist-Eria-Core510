12339823156ccca1264f8aecd14cf18284db673a
NoboDie <nobodie.dev@gmail.com>
2013-11-30 02:05:52 +0100

    dfdsfsfs

--- src/server/game/Battlegrounds/BattlegroundMgr.cpp
+++ src/server/game/Battlegrounds/BattlegroundMgr.cpp
@@ -168,9 +168,9 @@ void BattlegroundMgr::BuildBattlegroundStatusPacket(WorldPacket* data, Battlegro
         {
             data->Initialize(SMSG_BATTLEFIELD_STATUS);
 
-            *data << uint32(0);                         // unk, always 1 0 => testing ?
-            *data << uint32(QueueSlot);                 // Join Time
-            *data << uint32(0);                     // Queue slot
+            *data << uint32(1);                         // unk, always 1 0 => testing ?
+            *data << uint32(1);                 // Join Time
+            *data << uint32(1);                     // Queue slot
 
 
             sLog->outDebug(LOG_FILTER_NETWORKIO, "NOBODIE BGGUID : %u", bg?1:0);
