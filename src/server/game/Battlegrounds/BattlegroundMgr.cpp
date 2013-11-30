a6dd9e6d3078bc7cf70b5440deede8cd08917c9c
NoboDie <nobodie.dev@gmail.com>
2013-11-30 01:57:43 +0100

    one more try :)

--- src/server/game/Battlegrounds/BattlegroundMgr.cpp
+++ src/server/game/Battlegrounds/BattlegroundMgr.cpp
@@ -168,9 +168,9 @@ void BattlegroundMgr::BuildBattlegroundStatusPacket(WorldPacket* data, Battlegro
         {
             data->Initialize(SMSG_BATTLEFIELD_STATUS);
 
-            *data << uint32(QueueSlot);                         // unk, always 1 0 => testing ?
-            *data << uint32(0);                 // Queue slot
-            *data << uint32(0);                     // Join Time
+            *data << uint32(0);                         // unk, always 1 0 => testing ?
+            *data << uint32(0);                 // Join Time
+            *data << uint32(QueueSlot);                     // Queue slot
 
 
             sLog->outDebug(LOG_FILTER_NETWORKIO, "NOBODIE BGGUID : %u", bg?1:0);
