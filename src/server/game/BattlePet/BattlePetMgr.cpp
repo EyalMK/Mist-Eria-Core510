1a91cc71864a88464e30b005f4fe205a7cf7bb78
NoboDie <nobodie.dev@gmail.com>
2013-12-09 16:30:17 +0100

    compile

--- src/server/game/BattlePet/BattlePetMgr.cpp
+++ src/server/game/BattlePet/BattlePetMgr.cpp
@@ -131,7 +131,7 @@ void BattlePetMgr::BuildBattlePetJournal(WorldPacket *data)
 
         data->WriteBits(name.size(), 7); // name lenght
 
-        guid += 1;
+        guid = guid + 1;
     }
 
     data->WriteBits(0, 21); // unk counter, may be related to battle pet slot
@@ -175,7 +175,7 @@ void BattlePetMgr::BuildBattlePetJournal(WorldPacket *data)
         *data << uint32(petHealth);                       // petHealth
         *data << uint16(petLevel);                       // petlevel
 
-        guid += 1;
+        guid = guid + 1;
         //word14 ++;
         //word18 ++;
         //word1A ++;
