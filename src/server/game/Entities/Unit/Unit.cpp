c89eb760ae86f6a2302ddb892aab4bffe40ae031
Cristian Vlad <bordeianu.vlad@yahoo.com>
2013-12-09 14:13:15 +0200

    Warlock pets energy should be fixed ?

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -8403,12 +8403,9 @@ void Unit::SetMinion(Minion *minion, bool apply)
             for (uint8 i = 0; i < MAX_MOVE_TYPE; ++i)
                 minion->SetSpeed(UnitMoveType(i), m_speed_rate[i], true);
 
-        // Ghoul pets have energy instead of mana (is anywhere better place for this code?)
-        if (minion->IsPetGhoul())
+        // Ghoul pets and Warlock's pets have energy instead of mana (is anywhere better place for this code?)
+        if (minion->IsPetGhoul() || (minion->GetOwner() && minion->GetOwner()->getClass() == CLASS_WARLOCK))
             minion->setPowerType(POWER_ENERGY);
-
-		if (minion->GetOwner()->getClass() == CLASS_WARLOCK)
-			minion->setPowerType(POWER_ENERGY);
 
         if (GetTypeId() == TYPEID_PLAYER)
         {
