af1b90ee62ed913ce8b658cde17be72bcc66d26e
Sungis <Sungis.dev@gmail.com>
2013-12-18 16:17:34 +0100

    Oops ...

--- src/server/game/Battlegrounds/Zones/BattlegroundTK.cpp
+++ src/server/game/Battlegrounds/Zones/BattlegroundTK.cpp
@@ -114,7 +114,7 @@ void BattlegroundTK::CalculatePoints(uint32 diff)
 					if (player->GetTeam() == ALLIANCE && player->GetExactDist2d(1783.319336f, 1333.339722f) > 50.0f && player->GetExactDist2d(1783.319336f, 1333.339722f) <= 100.0f)
 					{
 						UpdateScore(ALLIANCE, BG_TK_INDOOR_POINTS);
-						UpdatePlayerScore(player, SCORE_POINTS_SCORED, BG_TK_CENTER_POINTS);
+						UpdatePlayerScore(player, SCORE_POINTS_SCORED, BG_TK_INDOOR_POINTS);
 					}
 
 				if (player->GetGUID() == m_orbOwners[0] ||
@@ -124,7 +124,7 @@ void BattlegroundTK::CalculatePoints(uint32 diff)
 					if (player->GetTeam() == HORDE && player->GetExactDist2d(1783.319336f, 1333.339722f) > 50.0f && player->GetExactDist2d(1783.319336f, 1333.339722f) <= 100.0f)
 					{
 						UpdateScore(HORDE, BG_TK_INDOOR_POINTS);
-						UpdatePlayerScore(player, SCORE_POINTS_SCORED, BG_TK_CENTER_POINTS);
+						UpdatePlayerScore(player, SCORE_POINTS_SCORED, BG_TK_INDOOR_POINTS);
 					}
 
 				if (player->GetGUID() == m_orbOwners[0] ||
@@ -134,7 +134,7 @@ void BattlegroundTK::CalculatePoints(uint32 diff)
 					if (player->GetTeam() == ALLIANCE && player->GetExactDist2d(1783.319336f, 1333.339722f) > 100.0f)
 					{
 						UpdateScore(ALLIANCE, BG_TK_OUTDOOR_POINTS);
-						UpdatePlayerScore(player, SCORE_POINTS_SCORED, BG_TK_CENTER_POINTS);
+						UpdatePlayerScore(player, SCORE_POINTS_SCORED, BG_TK_OUTDOOR_POINTS);
 					}
 
 				if (player->GetGUID() == m_orbOwners[0] ||
@@ -144,7 +144,7 @@ void BattlegroundTK::CalculatePoints(uint32 diff)
 					if (player->GetTeam() == HORDE && player->GetExactDist2d(1783.319336f, 1333.339722f) > 100.0f)
 					{
 						UpdateScore(HORDE, BG_TK_OUTDOOR_POINTS);
-						UpdatePlayerScore(player, SCORE_POINTS_SCORED, BG_TK_CENTER_POINTS);
+						UpdatePlayerScore(player, SCORE_POINTS_SCORED, BG_TK_OUTDOOR_POINTS);
 					}
 
 					pointsTimer = 4000;
