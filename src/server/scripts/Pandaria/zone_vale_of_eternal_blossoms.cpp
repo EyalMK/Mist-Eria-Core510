b755d3e0aca79bff61baa81e67a4a4cdbb1e1aea
NoboDie <nobodie.dev@gmail.com>
2014-01-03 22:46:36 +0100

    fix crash 2

--- src/server/scripts/Pandaria/zone_vale_of_eternal_blossoms.cpp
+++ src/server/scripts/Pandaria/zone_vale_of_eternal_blossoms.cpp
@@ -37,7 +37,7 @@ public:
 
         void MoveInLineOfSight(Unit* who)
         {
-            if (!me->IsWithinDistInMap(who, 40.0f) || (who->ToPlayer() && who->ToPlayer()->isGameMaster()) || who->HasAura(SPELL_CYCLONIC_INSPIRATION))
+            if (!me->IsWithinDistInMap(who, 40.0f) || (who && who->ToPlayer() && who->ToPlayer()->isGameMaster()) || who->HasAura(SPELL_CYCLONIC_INSPIRATION))
                 return;
 
 			if (!who || !who->IsInWorld() || !me->HasAura(SPELL_SPINNING_CRANE_KICK))
