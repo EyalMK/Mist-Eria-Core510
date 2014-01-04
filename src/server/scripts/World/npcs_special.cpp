b9fc4e2668d469349be715034ac8ccb40dde129f
Sungis <Sungis.dev@gmail.com>
2014-01-04 19:21:13 +0100

    Oooops (2)

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -2910,7 +2910,7 @@ public:
 			QueryResult result = WorldDatabase.Query("SELECT text FROM creature_text WHERE entry = 560130");
 
 			if (!result)
-			return;
+			return false;
 
 			std::string text;
 
