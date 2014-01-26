c29545c40c810e2775c525fd1174668cce165a41
Sylmir <sylmir.dev@gmail.com>
2014-01-26 11:56:06 +0100

    Fix vitesse de jump des trashs

--- src/server/scripts/Pandaria/StormstoutBrewery/boss_hoptallus.cpp
+++ src/server/scripts/Pandaria/StormstoutBrewery/boss_hoptallus.cpp
@@ -269,7 +269,7 @@ public :
                 if(Creature* summon = me->SummonCreature(entry, posSummon))
 				{
 					sLog->outDebug(LOG_FILTER_NETWORKIO, "Summoned virmen ; motion master");
-                    summon->GetMotionMaster()->MoveJump(posJump, 4.0f, 4.0f);
+                    summon->GetMotionMaster()->MoveJump(posJump, 8.0f, 8.0f);
 					sLog->outDebug(LOG_FILTER_NETWORKIO, "Jumped");
 				}
             }
