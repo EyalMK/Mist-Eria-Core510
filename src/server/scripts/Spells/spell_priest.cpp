66dbd1e23366307855c79ab9ade76b4852cd0cfe
Sylmir <sylmir.dev@gmail.com>
2014-02-07 23:33:42 +0100

    Done

--- src/server/scripts/Spells/spell_priest.cpp
+++ src/server/scripts/Spells/spell_priest.cpp
@@ -1170,8 +1170,8 @@ public :
                                 TypeContainerVisitor<Trinity::UnitListSearcher<Trinity::AnyUnfriendlyUnitInObjectRangeCheck>, WorldTypeMapContainer> visitor2(searcher);
 								
 								Map* map = creature->GetMap(); 
-                                cell.Visit(cell, visitor, *map, *creature, 84.0f);
-                                cell.Visit(cell, visitor2, *map, *creature, 84.0f);
+                                cell.Visit(coords, visitor, *map, *creature, 84.0f);
+                                cell.Visit(coords, visitor2, *map, *creature, 84.0f);
 
                                 if(!targets.empty()) {
                                     targets.sort(Trinity::DistanceCompareOrderPred(player, true));
