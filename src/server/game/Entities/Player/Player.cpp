1d5dd118a3e5c033cc4871740273ceeb5e488e8a
tweex <bdhuiege@gmail.com>
2014-02-17 22:21:11 +0100

    Revert

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -961,9 +961,6 @@ void Player::CleanupsBeforeDelete(bool finalCleanup)
     TradeCancel(false);
     DuelComplete(DUEL_INTERRUPTED);
 
-    if(this->HasAura(102938))
-        this->RemoveAurasDueToSpell(102938);
-
     Unit::CleanupsBeforeDelete(finalCleanup);
 
     if (m_transport)
