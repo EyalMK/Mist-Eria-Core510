0d16338004bec17bb537dde265d7b02b941c3618
Sungis18 <Sungis.dev@gmail.com>
2014-02-19 20:03:25 +0100

    Silvershard Mines : Testing worldstates

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -10245,17 +10245,17 @@ void Player::SendInitWorldStates(uint32 zoneid, uint32 areaid)
 				bg->FillInitialWorldStates(data);
 			else
 			{
-				data << uint32(0x1924) << uint32(0x0);
-				data << uint32(0x1925) << uint32(0x0);
-				data << uint32(0x1926) << uint32(0x0);
-				data << uint32(0x1927) << uint32(0x0);
-				data << uint32(0x1928) << uint32(0x0);
-				data << uint32(0x1929) << uint32(0x0);
-				data << uint32(0x192a) << uint32(0x0);
-				data << uint32(0x192b) << uint32(0x0);
-				data << uint32(0x1adb) << uint32(0x0);
-				data << uint32(0x1adc) << uint32(0x0);
-				data << uint32(0x1add) << uint32(0x0);
+				data << uint32(0x1924) << uint32(1);
+				data << uint32(0x1925) << uint32(50);
+				data << uint32(0x1926) << uint32(60);
+				data << uint32(0x1927) << uint32(1);
+				data << uint32(0x1928) << uint32(1);
+				data << uint32(0x1929) << uint32(1);
+				data << uint32(0x192a) << uint32(1);
+				data << uint32(0x192b) << uint32(1);
+				data << uint32(0x1adb) << uint32(1);
+				data << uint32(0x1adc) << uint32(1);
+				data << uint32(0x1add) << uint32(1);
 			}
 			break;
 		// Tol'Viron Arena
