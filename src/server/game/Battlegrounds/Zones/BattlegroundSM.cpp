3eeb7ce7b2b56127a2e2d0456272c7df50ae4145
Sungis18 <Sungis.dev@gmail.com>
2014-02-23 12:27:00 +0100

    Silvershard Mines : Working on worldstates

--- src/server/game/Battlegrounds/Zones/BattlegroundSM.cpp
+++ src/server/game/Battlegrounds/Zones/BattlegroundSM.cpp
@@ -1271,7 +1271,7 @@ void BattlegroundSM::FillInitialWorldStates(WorldPacket& data)
 	data << uint32(SM_ALLIANCE_RESOURCES) << uint32(m_TeamScores[TEAM_HORDE]);
 	data << uint32(SM_HORDE_RESOURCES) << uint32(m_TeamScores[TEAM_ALLIANCE]);
 	data << uint32(SM_UNK2) << uint32(1);
-	data << uint32(SM_UNK3) << uint32(1);
+	data << uint32(SM_UNK3) << uint32(0);
 	data << uint32(SM_UNK4) << uint32(1);
 	data << uint32(SM_DISPLAY_ALLIANCE_RESSOURCES) << uint32(1);
 	data << uint32(SM_DISPLAY_HORDE_RESSOURCES) << uint32(1);
