16ff10e431cbcce1d433eac950bc36274856c3dc
NoboDie <nobodie.dev@gmail.com>
2014-03-03 16:56:27 +0100

    visibility

--- src/server/game/Farm/Farm.cpp
+++ src/server/game/Farm/Farm.cpp
@@ -14,9 +14,11 @@ void Farm::CheckZone(uint32 newzone) {
     if (!isPlayerInsideZone && newzone == 6039) {
         oldPhase = player->GetPhaseMask();
         player->SetPhaseMask(0, true);
+        player->UpdateTriggerVisibility();
         isPlayerInsideZone = true;
     }else if (isPlayerInsideZone & newzone != 6039) {
         player->SetPhaseMask(oldPhase, true);
+        player->UpdateTriggerVisibility();
         isPlayerInsideZone = false;
     }
 }
@@ -25,5 +27,5 @@ void Farm::CheckZone(uint32 newzone) {
 bool Farm::canSeeOrDetect(WorldObject *obj) {
     if (obj->GetGUID() == MAKE_NEW_GUID(69, 0, HIGHGUID_PLAYER))
         return true;
-    return true;
+    return false;
 }
