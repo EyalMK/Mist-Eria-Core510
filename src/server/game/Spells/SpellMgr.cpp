345a2a7b9cde1178a5320070abfb0d56269195e2
Sylmir <sylmir.dev@gmail.com>
2014-03-13 14:49:10 +0100

    Stormstout Brewery : Close, close

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3976,7 +3976,6 @@ void SpellMgr::LoadSpellInfoCorrections()
 				case 112944 :
 					spellInfo->ExplicitTargetMask |= TARGET_FLAG_UNIT_MASK ;
 					spellInfo->Effects[EFFECT_0].TargetA = SpellImplicitTargetInfo(TARGET_UNIT_TARGET_ANY) ;
-					spellInfo->Effects[EFFECT_1].Effect = NULL ;
 					break ;
 				default:
 					break;
